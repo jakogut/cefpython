1dac04739219ddab911878e7976057506bfa297a
Joseph Kogut <joseph.kogut@gmail.com>
2018-08-13 10:14:18 -0700

    update patches for 69.0

--- patches/patch.py
+++ patches/patch.py
@@ -13,31 +13,10 @@ OS_POSTFIX = ("win" if platform.system() == "Windows" else
 if OS_POSTFIX == "linux":
     # noinspection PyUnresolvedReferences
     patches.extend([
-        # Chromium build issues
-        {
-            'name': '0002-chromium-compiler-r2',
-            'path': '.'
-        },
         #{
-        #    'name': '0003-ffmpeg-r1',
+        #    'name': 'vaapi',
         #    'path': '.'
         #},
-        {
-            'name': '0004-libwebp-shim-r0',
-            'path': '.'
-        },
-        {
-            'name': '0005-cors-string-r0',
-            'path': '.'
-        },
-        {
-            'name': '0006-libjpeg-r0',
-            'path': '.'
-        },
-        {
-            'name': 'vaapi',
-            'path': '.'
-        },
         {
             # Discovery of the "icudtl.dat" file fails on Linux.
             'name': 'issue231',
